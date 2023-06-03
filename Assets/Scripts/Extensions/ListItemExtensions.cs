6adec07f58e56e433432dddffd5a3c14ebada900
AmirBarak <binary.eyes.dev@gmail.com>
2023-06-04 02:56:53 +0300

    added list item extensions

---
+++ Assets/Scripts/Extensions/ListItemExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace QueueGame.Extensions
+{
+    public static class ListItemExtensions
+    {
+        public static T GetRandom<T>(this IReadOnlyList<T> list)
+        {
+            var index = Random.Range(0, list.Count);
+            return list[index];
+        }
+    }
+}
