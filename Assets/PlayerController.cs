9df5cca0a4ed7ee0958cc509baf0fc8b3c15e6f7
AmirBarak <binary.eyes.dev@gmail.com>
2023-06-03 23:10:30 +0300

    player will turn based on movement direction

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,18 +1,35 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 10.0f;
+    private Transform _transform;
     private Vector2 moveDirection;
+    public float speed = 10.0f;
+    public int Facing = 1;
 
     void Update()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-
         moveDirection = new Vector2(moveHorizontal, moveVertical);
 
+        if (Mathf.Abs(moveHorizontal) > 0.0f)
+        {
+            var horizontalSign = (int)Mathf.Sign(moveHorizontal);
+            Facing = -horizontalSign;
+        }
+
         // Ensure that this script only controls its own GameObject
-        transform.position += (Vector3)moveDirection * speed * Time.deltaTime;
+        _transform.position += (Vector3)moveDirection*speed*Time.deltaTime;
+
+        var scale = _transform.localScale;
+        scale.x = Facing;
+        _transform.localScale = scale;
+    }
+
+    private void Awake()
+    {
+        _transform = GetComponent<Transform>();
     }
 }
