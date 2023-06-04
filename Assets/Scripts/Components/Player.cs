d9c698b18575274ebb7ad4bff8abc9a3d05dd734
AmirBarak <binary.eyes.dev@gmail.com>
2023-06-04 15:57:11 +0300

    trying to understand player controls

--- Assets/Scripts/Components/Player.cs
+++ Assets/Scripts/Components/Player.cs
@@ -5,6 +5,34 @@ namespace QueueGame.Components
     public class Player
         : MonoBehaviour
     {
+        [Header("State")]
+        [SerializeField] private PlayerState _state;
+        [SerializeField] private Transform _bodyPivot;
+        [SerializeField] private Transform _neckPivot;
+        [SerializeField] private Camera _camera;
+        public PlayerState State => _state;
 
+        private void Awake()
+            => _state = PlayerState.Standing;
+
+        public void SetAction(PlayerAction action)
+        {
+
+        }
+    }
+
+    public enum PlayerAction
+    {
+        None,
+        LeanLeft,
+        LeanRight,
+    }
+
+    public enum PlayerState
+    {
+        Standing,
+        LeaningLeft,
+        LeaningRight,
+        StraighteningOut,
     }
 }
--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -6,7 +6,18 @@ namespace QueueGame.Managers
     public class PlayerManager
         : SingletonManager<PlayerManager>
     {
+        [Header("State")]
         [SerializeField] private Player _player;
         public Player CurrentPlayer => _player;
+
+        [Header("Input")]
+        [SerializeField] private KeyCode LeanLeftKey = KeyCode.A;
+        [SerializeField] private KeyCode LeanRightKey = KeyCode.D;
+        [SerializeField] private KeyCode MoveForwardKey = KeyCode.W;
+
+        public void Initialize()
+        {
+            Debug.Log("InitializingPlayerManager");
+        }
     }
 }
