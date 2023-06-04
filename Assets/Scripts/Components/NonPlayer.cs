13d4a7868d225112baf50a0e8a7c51e96c97d498
AmirBarak <binary.eyes.dev@gmail.com>
2023-06-04 22:45:37 +0300

    updated tapping mechanism

--- Assets/Scripts/Components/NonPlayer.cs
+++ Assets/Scripts/Components/NonPlayer.cs
@@ -1,4 +1,6 @@
-﻿using QueueGame.Enums;
+﻿using BinaryEyes.Common.Attributes;
+using BinaryEyes.Common.Extensions;
+using QueueGame.Enums;
 using UnityEngine;
 
 namespace QueueGame.Components
@@ -6,7 +8,14 @@ namespace QueueGame.Components
     public class NonPlayer
         : MonoBehaviour
     {
-        [SerializeField] private NonPlayerState _state;
+        [SerializeField] [ReadOnlyField] private NonPlayerState _state;
+        [SerializeField] private Transform _turnPivot;
         public NonPlayerState State => _state;
+
+        public void EngageWithPlayer()
+        {
+            this.LogMessage("EngagingWithPlayer");
+            _state = NonPlayerState.EngagedWithPlayer;
+        }
     }
 }
--- Assets/Scripts/Components/NonPlayerTapArea.cs
+++ Assets/Scripts/Components/NonPlayerTapArea.cs
@@ -1,5 +1,5 @@
 ﻿using BinaryEyes.Common.Attributes;
-using BinaryEyes.Common.Extensions;
+using QueueGame.Enums;
 using UnityEngine;
 
 namespace QueueGame.Components
@@ -9,15 +9,38 @@ namespace QueueGame.Components
     {
         [SerializeField] [ReadOnlyField] private int _tapCount;
         [SerializeField] [ReadOnlyField] private float _tapTimeLeft;
+        private NonPlayer _owner;
 
         private void OnMouseDown()
         {
-            this.LogMessage("MouseDown");
+            if (_owner.State != NonPlayerState.Waiting)
+                return;
+
+            _tapCount += 1;
+            _tapTimeLeft = 0.5f;
+            if (_tapCount < 2)
+                return;
+
+            _owner.EngageWithPlayer();
         }
 
         private void Update()
         {
+            if (_tapCount == 0)
+                return;
 
+            _tapTimeLeft -= Time.deltaTime;
+            if (_tapTimeLeft > 0.0f)
+                return;
+             
+            _tapCount -= 1;
+            if (_tapCount > 0)
+                _tapTimeLeft = 0.5f;
+        }
+
+        private void Awake()
+        {
+            _owner = GetComponentInParent<NonPlayer>();
         }
     }
 }
