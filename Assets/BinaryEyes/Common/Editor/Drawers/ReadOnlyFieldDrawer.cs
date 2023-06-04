720ffb069846977e2955958872b144a589f5cf40
AmirBarak <binary.eyes.dev@gmail.com>
2023-06-04 22:33:43 +0300

    added readonlyfield attribute

---
+++ Assets/BinaryEyes/Common/Editor/Drawers/ReadOnlyFieldDrawer.cs
@@ -0,0 +1,20 @@
+﻿using BinaryEyes.Common.Attributes;
+using UnityEngine;
+using UnityEditor;
+
+namespace BinaryEyes.Common.Drawers
+{
+    [CustomPropertyDrawer(typeof(ReadOnlyFieldAttribute))]
+    public class ReadOnlyFieldAttributeDrawer
+        : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var previousGuiState = GUI.enabled;
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label);
+
+            GUI.enabled = previousGuiState;
+        }
+    }
+}
---
+++ Assets/BinaryEyes/Common/Scripts/Attributes/ReadOnlyFieldAttribute.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace BinaryEyes.Common.Attributes
+{
+    /// <summary>
+    /// Used to mark a field as "readonly" in the editor.
+    /// </summary>
+    public class ReadOnlyFieldAttribute
+        : PropertyAttribute
+    {
+
+    }
+}
