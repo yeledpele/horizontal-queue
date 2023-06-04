3d606348780fdc57fcbeb2524c44820569ddd0ff
AmirBarak <binary.eyes.dev@gmail.com>
2023-06-04 03:00:42 +0300

    added the name dictionary asset

---
+++ Assets/Scripts/Data/NameDictionary.cs
@@ -0,0 +1,20 @@
+﻿using QueueGame.Enums;
+using QueueGame.Extensions;
+using UnityEngine;
+
+namespace QueueGame.Data
+{
+    [CreateAssetMenu(menuName = "Queues/Name Dictionary")]
+    public class NameDictionary
+        : ScriptableObject
+    {
+        [SerializeField] private string[] _maleNames;
+        [SerializeField] private string[] _femaleNames;
+
+        public string GetRandomName(Gender gender)
+        {
+            var list = gender == Gender.Male ? _maleNames : _femaleNames;
+            return list.GetRandom();
+        }
+    }
+}
