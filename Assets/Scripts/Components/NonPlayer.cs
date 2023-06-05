fff84db9c022d3b642b3b450bd65b51075bf6ef1
AmirBarak <binary.eyes.dev@gmail.com>
2023-06-05 18:49:44 +0300

    npcs will lose interest and turn away from player if no interactions are detected

--- Assets/Scripts/Components/NonPlayer.cs
+++ Assets/Scripts/Components/NonPlayer.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using BinaryEyes.Common.Attributes;
 using BinaryEyes.Common.Extensions;
 using QueueGame.Enums;
-using QueueGame.Managers;
 using UnityEngine;
 
 namespace QueueGame.Components
@@ -11,29 +10,63 @@ namespace QueueGame.Components
     public class NonPlayer
         : MonoBehaviour
     {
+        [Header("State")]
         [SerializeField] [ReadOnlyField] private NonPlayerState _state;
+        [SerializeField] [ReadOnlyField] private float _currentAttentionSpan;
+
+        [Header("Rotation")]
         [SerializeField] private float _rotationTime;
         [SerializeField] private Transform _turnPivot;
-        private Coroutine _facingProcess;
+
+        [Header("Attention")]
+        [SerializeField] private float _attentionSpan;
+        [SerializeField] private float _attentionSpanDepletionRate;
+        private Coroutine _currentProcess;
         public NonPlayerState State => _state;
+        public float CurrentAttentionSpan => _currentAttentionSpan;
 
         public void EngageWithPlayer()
         {
-            if (_state == NonPlayerState.EngagedWithPlayer)
-                throw new InvalidOperationException("non-player is already engaged");
+            if (_state != NonPlayerState.Waiting)
+                throw new InvalidOperationException($"failed to engage with player: npcState={State}");
 
-            this.LogMessage("EngagingWithPlayer");
-            _state = NonPlayerState.EngagedWithPlayer;
-            _facingProcess = StartCoroutine(FacePlayer());
+            _state = NonPlayerState.Turning;
+            _currentProcess = StartCoroutine(FacePlayer());
         }
 
         private IEnumerator FacePlayer()
         {
+            this.LogMessage("FacingPlayer");
             var source = _turnPivot.position;
             var rotationDirection = source.x < 0.0f ? +1.0f : -1.0f;
             var targetRotation = rotationDirection*175.0f;
             var tween = LeanTween.rotateY(_turnPivot.gameObject, targetRotation, _rotationTime);
             yield return new WaitWhile(() => LeanTween.isTweening(tween.uniqueId));
+
+            this.LogMessage("EngagedWithPlayer");
+            _state = NonPlayerState.EngagedWithPlayer;
+            _currentProcess = StartCoroutine(LoseInterestInPlayer());
+        }
+
+        private IEnumerator LoseInterestInPlayer()
+        {
+            _currentAttentionSpan = _attentionSpan;
+            while (_currentAttentionSpan > 0.0f)
+            {
+                yield return null;
+                _currentAttentionSpan -= _attentionSpanDepletionRate*Time.deltaTime;
+            }
+
+            this.LogMessage("LostInterestInPlayer");
+            _state = NonPlayerState.Turning;
+            var source = _turnPivot.position;
+            var rotationDirection = source.x < 0.0f ? -1.0f : +1.0f;
+            var targetRotation = rotationDirection*1.0f;
+            var tween = LeanTween.rotateY(_turnPivot.gameObject, targetRotation, _rotationTime);
+            yield return new WaitWhile(() => LeanTween.isTweening(tween.uniqueId));
+
+            this.LogMessage("WaitingInLine");
+            _state = NonPlayerState.Waiting;
         }
     }
 }
--- Assets/Scripts/Enums/NonPlayerState.cs
+++ Assets/Scripts/Enums/NonPlayerState.cs
@@ -3,6 +3,7 @@
     public enum NonPlayerState
     {
         Waiting,
+        Turning,
         EngagedWithPlayer,
     }
 }
